126482285f61911c47c515642b0020d5bb1f307d
yulaw-k <1065931263@qq.com>
2021-10-20 22:50:10 +0800

    第十三次

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 mvn clean
 mvn install
-nohup java  -Xms256m -Xmx256m -jar ./package/ccb-api-0.0.1-SNAPSHOT.jar &
+nohup java  -Xms256m -Xmx256m -jar ./package/ccb-api-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
