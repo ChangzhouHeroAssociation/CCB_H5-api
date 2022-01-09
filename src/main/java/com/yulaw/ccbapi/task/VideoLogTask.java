c1d184711bd7763480f86cfa78e298b38d61125b
sunwei19910119 <754293591@qq.com>
2022-01-09 09:41:19 +0800

    Update VideoLogTask.java

--- src/main/java/com/yulaw/ccbapi/task/VideoLogTask.java
+++ src/main/java/com/yulaw/ccbapi/task/VideoLogTask.java
@@ -14,13 +14,13 @@ public class VideoLogTask {
     @Autowired
     VideoLogService videoLogService;
 
-    @Scheduled(cron = "0 0/30 * * * * ?")
+    @Scheduled(cron = "0 0/30 * * * ?")
     public void updateVideoLog(){
         videoLogService.task();
         System.out.println("更新视频日志！");
     }
 
-    @Scheduled(cron = "0 0/30 * * * * ?")
+    @Scheduled(cron = "0 0/30 * * * ?")
     public void updateVideoData(){
         videoLogService.update();
         System.out.println("更新视频统计！");
